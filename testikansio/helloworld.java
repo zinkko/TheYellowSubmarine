2664b9624c3dec99caa1ec67db56866c1300abba
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2014-03-12 14:23:11 +0200

    uusi testitiedosto ja -kansio lisätty

---
+++ testikansio/helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld{
+	public static void main(String[] args){
+		if (args.length>0){
+			System.out.println("hello "+args[0]+ "!");	
+		}else{
+			System.out.println("hello world!");
+		}
+	}
+}
