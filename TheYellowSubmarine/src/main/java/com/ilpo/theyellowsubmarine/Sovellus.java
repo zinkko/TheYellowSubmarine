144594f3e1940dcabe98be1f8cc02fa9620e634e
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2014-03-14 21:55:18 +0200

    luokkarunko alkaa muotoutua\!

---
+++ TheYellowSubmarine/src/main/java/com/ilpo/theyellowsubmarine/Sovellus.java
@@ -0,0 +1,17 @@
+package com.ilpo.theyellowsubmarine;
+
+import com.ilpo.theyellowsubmarine.kayttoliittyma.Kayttoliittyma;
+import com.ilpo.theyellowsubmarine.logiikka.SovellusOhjain;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sovellus {
+    private Kayttoliittyma kayttoliittyma;
+    private SovellusOhjain kontrolleri;
+            
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
